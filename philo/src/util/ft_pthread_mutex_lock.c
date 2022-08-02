38afafa6582b4eaa7b6058d17df5015134dc2bd0
osorosia <64416398+osorosia@users.noreply.github.com>
2022-08-02 18:21:20 +0900

    fix bug

--- philo/src/util/ft_pthread_mutex_lock.c
+++ philo/src/util/ft_pthread_mutex_lock.c
@@ -3,7 +3,7 @@
 int ft_pthread_mutex_lock(pthread_mutex_t *mutex) {
     int ret;
     
-    ret = pthread_mutex_unlock(mutex);
+    ret = pthread_mutex_lock(mutex);
     if (ret != 0)
         printf("pthread_mutex_lock: error\n");
     return ret;
