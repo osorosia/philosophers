e6da3f8b37a25bc37edc31a3510e34520131fdac
osorosia <64416398+osorosia@users.noreply.github.com>
2022-08-03 10:37:38 +0900

    norm

--- philo/src/action.c
+++ philo/src/action.c
@@ -1,68 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   action.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/03 10:28:50 by rnishimo          #+#    #+#             */
+/*   Updated: 2022/08/03 10:32:18 by rnishimo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-bool print_action(t_philo *philo, t_action_kind act_kind) {
-    const char *act[] = {
-        "is eating", "is sleeping", "is thinking", "has taken a fork",
-    };
+bool	print_action(t_philo *philo, t_action_kind act_kind)
+{
+	const char	*act[] = {
+		"is eating", "is sleeping", "is thinking", "has taken a fork",
+	};
 
-    ft_pthread_mutex_lock(&philo->table->print_mutex);
-    philo->prev_act_time = get_timestamp();
-    if (philo->table->finish) {
-        ft_pthread_mutex_unlock(&philo->table->print_mutex);
-        return false;
-    }
-    printf("%ld\t%ld %s\n", philo->prev_act_time - philo->table->start_time,
-           philo->id + 1, act[act_kind]);
-    ft_pthread_mutex_unlock(&philo->table->print_mutex);
-    return true;
+	ft_pthread_mutex_lock(&philo->table->print_mutex);
+	philo->prev_act_time = get_timestamp();
+	if (philo->table->finish)
+	{
+		ft_pthread_mutex_unlock(&philo->table->print_mutex);
+		return (false);
+	}
+	printf("%ld\t%ld %s\n", philo->prev_act_time - philo->table->start_time,
+		   philo->id + 1, act[act_kind]);
+	ft_pthread_mutex_unlock(&philo->table->print_mutex);
+	return (true);
 }
 
-bool action_eat(t_philo *philo) {
-    long timestamp = get_timestamp();
-    if (!print_action(philo, EAT))
-        return false;
+bool	action_eat(t_philo *philo)
+{
+	long	timestamp;
 
-    ft_pthread_mutex_lock(&philo->table->eat_time_mutex[philo->id]);
-    philo->prev_eat_time = timestamp;
-    ft_pthread_mutex_unlock(&philo->table->eat_time_mutex[philo->id]);
+	timestamp = get_timestamp();
+	if (!print_action(philo, EAT))
+		return (false);
+	ft_pthread_mutex_lock(&philo->table->eat_time_mutex[philo->id]);
+	philo->prev_eat_time = timestamp;
+	ft_pthread_mutex_unlock(&philo->table->eat_time_mutex[philo->id]);
 
-    ft_pthread_mutex_lock(&philo->table->eat_count_mutex[philo->id]);
-    philo->eat_count++;
-    ft_pthread_mutex_unlock(&philo->table->eat_count_mutex[philo->id]);
-    return true;
+	ft_pthread_mutex_lock(&philo->table->eat_count_mutex[philo->id]);
+	philo->eat_count++;
+	ft_pthread_mutex_unlock(&philo->table->eat_count_mutex[philo->id]);
+	return (true);
 }
 
-bool action_sleep(t_philo *philo) {
-    long timestamp = get_timestamp();
-    while (timestamp - philo->prev_act_time < philo->rule->eat) {
-        usleep(100);
-        timestamp = get_timestamp();
-    }
-    return print_action(philo, SLEEP);
+bool	action_sleep(t_philo *philo)
+{
+	long	timestamp;
+
+	timestamp = get_timestamp();
+	while (timestamp - philo->prev_act_time < philo->rule->eat)
+	{
+		usleep(100);
+		timestamp = get_timestamp();
+	}
+	return (print_action(philo, SLEEP));
 }
 
-bool action_think(t_philo *philo) {
-    long timestamp = get_timestamp();
-    while (timestamp - philo->prev_act_time < philo->rule->sleep) {
-        usleep(100);
-        timestamp = get_timestamp();
-    }
-    return print_action(philo, THINK);
+bool	action_think(t_philo *philo)
+{
+	long timestamp;
+
+	timestamp = get_timestamp();
+	while (timestamp - philo->prev_act_time < philo->rule->sleep)
+	{
+		usleep(100);
+		timestamp = get_timestamp();
+	}
+	return (print_action(philo, THINK));
 }
 
-bool action_get_fork(t_philo *philo) {
-    long timestamp = get_timestamp();
-    return print_action(philo, GET_FORK);
+bool	action_get_fork(t_philo *philo)
+{
+	return (print_action(philo, GET_FORK));
 }
 
-bool action(t_philo *philo, t_action_kind act_kind) {
-    if (act_kind == EAT)
-        return action_eat(philo);
-    else if (act_kind == SLEEP)
-        return action_sleep(philo);
-    else if (act_kind == THINK)
-        return action_think(philo);
-    else if (act_kind == GET_FORK)
-        return action_get_fork(philo);
-    return false;
+bool	action(t_philo *philo, t_action_kind act_kind)
+{
+	if (act_kind == EAT)
+		return (action_eat(philo));
+	else if (act_kind == SLEEP)
+		return (action_sleep(philo));
+	else if (act_kind == THINK)
+		return (action_think(philo));
+	else if (act_kind == GET_FORK)
+		return (action_get_fork(philo));
+	return (false);
 }
--- philo/src/philo.h
+++ philo/src/philo.h
@@ -1,99 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnishimo <rnishimo@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/03 10:32:37 by rnishimo          #+#    #+#             */
+/*   Updated: 2022/08/03 10:36:09 by rnishimo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILO_H
-#define PHILO_H
+# define PHILO_H
 
-#include <limits.h>
-#include <pthread.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/time.h>
-#include <unistd.h>
+# include <limits.h>
+# include <pthread.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
 
-#ifndef DEBUG
-#define DEBUG 0
-#endif
+# ifndef DEBUG
+#  define DEBUG 0
+# endif
 
 typedef enum e_action_kind {
-    EAT,
-    SLEEP,
-    THINK,
-    GET_FORK,
-} t_action_kind;
+	EAT,
+	SLEEP,
+	THINK,
+	GET_FORK,
+}	t_action_kind;
 
-typedef struct s_rule t_rule;
-typedef struct s_philo t_philo;
-typedef struct s_table t_table;
-typedef struct s_monitor t_monitor;
+typedef struct s_rule		t_rule;
+typedef struct s_philo		t_philo;
+typedef struct s_table		t_table;
+typedef struct s_monitor	t_monitor;
 
 struct s_rule {
-    long philo_num;
-    long die;
-    long eat;
-    long sleep;
-    long eat_count;
+	long	philo_num;
+	long	die;
+	long	eat;
+	long	sleep;
+	long	eat_count;
 };
 
 struct s_philo {
-    long id;
-    t_rule *rule;
-    t_table *table;
+	long			id;
+	t_rule			*rule;
+	t_table			*table;
 
-    t_action_kind next_act_kind;
-    long prev_eat_time;
-    long prev_act_time;
-    long eat_count;
+	t_action_kind	next_act_kind;
+	long			prev_eat_time;
+	long			prev_act_time;
+	long			eat_count;
 };
 
 struct s_table {
-    long id;
-    t_philo **philos;
-    pthread_mutex_t *forks;
-    pthread_mutex_t *eat_time_mutex;
-    pthread_mutex_t *eat_count_mutex;
-    pthread_mutex_t print_mutex;
+	long			id;
+	t_philo			**philos;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	*eat_time_mutex;
+	pthread_mutex_t	*eat_count_mutex;
+	pthread_mutex_t	print_mutex;
 
-    t_rule *rule;
-    long start_time;
+	t_rule			*rule;
+	long			start_time;
 
-    bool finish;
+	bool			finish;
 };
 
 struct s_monitor {
-    t_table *table;
-    t_rule *rule;
-    t_philo **philos;
+	t_table		*table;
+	t_rule		*rule;
+	t_philo		**philos;
 
-    pthread_t *philo_th;
+	pthread_t	*philo_th;
 };
 
 // action.c
-bool action(t_philo *philo, t_action_kind act_kind);
+bool	action(t_philo *philo, t_action_kind act_kind);
 
 // monitor_routine.c
-void *monitor_routine(void *arg);
+void	*monitor_routine(void *arg);
 
 // philo_routine.c
-void *philo_routine(void *arg);
+void	*philo_routine(void *arg);
 
 // table.c
-t_table *table_new();
-void table_free(t_table *table);
+t_table	*table_new(int argc, char **argv);
+void	table_free(t_table *table);
 
 //
 // util
 //
-void error(char *str);
-long ft_atol(char *str);
-void *ft_calloc(size_t count, size_t size);
-bool ft_isdigit(char c);
-int ft_pthread_create(pthread_t *th, const pthread_attr_t *attr,
-                      void *(*start_routine)(void *), void *arg);
-int ft_pthread_join(pthread_t th, void **value_ptr);
-void ft_pthread_mutex_destroy(pthread_mutex_t *mutex);
-int ft_pthread_mutex_lock(pthread_mutex_t *mutex);
-int ft_pthread_mutex_unlock(pthread_mutex_t *mutex);
-long get_timestamp();
-bool is_num(char *str);
+void	error(char *str);
+long	ft_atol(char *str);
+void	*ft_calloc(size_t count, size_t size);
+bool	ft_isdigit(char c);
+int		ft_pthread_create(pthread_t *th, const pthread_attr_t *attr, \
+					  void *(*start_routine)(void *), void *arg);
+int		ft_pthread_join(pthread_t th, void **value_ptr);
+void	ft_pthread_mutex_destroy(pthread_mutex_t *mutex);
+int		ft_pthread_mutex_lock(pthread_mutex_t *mutex);
+int		ft_pthread_mutex_unlock(pthread_mutex_t *mutex);
+long	get_timestamp(void);
+bool	is_num(char *str);
 
 #endif
