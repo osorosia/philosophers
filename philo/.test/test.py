2f6a1097180e5d22addf35491c6530158913e127
osorosia <64416398+osorosia@users.noreply.github.com>
2022-08-03 17:47:31 +0900

    fix test

--- philo/.test/test.py
+++ philo/.test/test.py
@@ -27,49 +27,73 @@ def get_act_kind(strs):
         return ACT.GET_FORK
     if strs[2] == "died":
         return ACT.DIE
+    print("act_kind")
     exit(1)
+
+mp = {
+    ACT.EAT: 'eat',
+    ACT.SLEEP: 'sleep',
+    ACT.THINK: 'think',
+    ACT.GET_FORK: 'get_fork',
+    ACT.DIE: 'die',
+}
 
 def check_philo(lines, target_id):
     prev_act_time = 0
     prev_eat_time = 0
     prev_act_kind = ACT.THINK
     target_eat_count = 0
-    fork = 0
+    fork_count = 0
+    after_die = False
 
     for strs in lines:
+        if after_die:
+            print("too long")
+            exit(1)
         timestamp = int(strs[0])
-        target_id = int(strs[1])
+        id = int(strs[1])
         if id != target_id:
             continue
 
+
         act_kind = get_act_kind(strs)
+
         if act_kind != ACT.DIE and timestamp - prev_eat_time > die:
+            print("over time")
             exit(1)
         if act_kind == ACT.EAT:
-            if prev_act_kind != ACT.THINK or fork != 2:
+            if prev_act_kind != ACT.GET_FORK or fork_count != 2:
+                print("eat")
                 exit(1)
             prev_eat_time = timestamp
-            fork = 0
+            fork_count = 0
             target_eat_count += 1
         if act_kind == ACT.SLEEP:
             if prev_act_kind != ACT.EAT or timestamp - prev_act_time < eat:
+                print("sleep")
                 exit(1)
         if act_kind == ACT.THINK:
             if prev_act_kind != ACT.SLEEP or timestamp - prev_act_time < sleep:
+                print("think")
                 exit(1)
         if act_kind == ACT.GET_FORK:
-            if prev_act_kind == ACT.THINK and fork == 0 \
-                or prev_act_kind == ACT.GET_FORK and fork == 1 :
-                fork += 1
+            if prev_act_kind == ACT.THINK and fork_count == 0:
+                fork_count = 1
+            elif prev_act_kind == ACT.GET_FORK and fork_count == 1:
+                fork_count = 2
             else:
+                print("get_fork", fork_count, mp[prev_act_kind])
                 exit(1)
         if act_kind == ACT.DIE:
             if die <= timestamp - prev_eat_time <= die + 10:
-                pass
+                after_die = True
             else:
+                print("die")
                 exit(1)
         prev_act_time = timestamp
-    if target_eat_count < eat_count:
+        prev_act_kind = act_kind
+    if not after_die and target_eat_count < eat_count:
+        print(target_eat_count, '<', eat_count);
         exit(1)
 
 if __name__ == '__main__':
--- philo/.test/test.sh
+++ philo/.test/test.sh
@@ -4,45 +4,29 @@ RED='\033[0;31m'
 GREEN='\033[32m'
 NC='\033[0m'
 
-function CHECK() {
-  testcase="$1"
-  ../philo $2 $3 $4 $5 $6 > "log/${testcase}.txt"
-  python3 test.py "log/${testcase}.txt" "$2" "$3" "$4" "$5" "$6"
+function CHECK_DIED() {
+  testcase="${1}_${2}_${3}_${4}_${5}"
+  ../philo $1 $2 $3 $4 $5 > "log/${testcase}.txt"
 
+  python3 test.py "log/${testcase}.txt" "$1" "$2" "$3" "$4" "$5"
   result=$?
-  if [ "$result" = "0" ]; then
-    echo -e "${GREEN}OK:${NC} ${testcase}"
-  else
-    echo -e "${RED}KO:${NC} ${testcase}"
-  fi
-
-}
-
-function CHECK_DIE() {
-  testcase="$1"
-  ../philo $2 $3 $4 $5 $6 > "log/${testcase}.txt"
-  python3 test.py "log/${testcase}.txt" "$2" "$3" "$4" "$5" "$6"
 
   cat "log/${testcase}.txt" | grep died > /dev/null
-  result1=$?
-  if [ "$result" = "0" ] && [ "$result1" != "0" ]; then
-    result=1
-  fi
-
   result=$?
+
   if [ "$result" = "0" ]; then
-    echo -e "${GREEN}OK:${NC} ${testcase}"
+    echo -e "${GREEN}DIED:${NC} $1 $2 $3 $4 $5"
   else
-    echo -e "${RED}KO:${NC} ${testcase}"
+    echo -e "${RED}LIVE:${NC} $1 $2 $3 $4 $5"
   fi
 
 }
 
 function CHECK_LIVE() {
-  testcase="$1"
-  ../philo $2 $3 $4 $5 $6 > "log/${testcase}.txt"
+  testcase="${1}_${2}_${3}_${4}_${5}"
+  ../philo $1 $2 $3 $4 $5 > "log/${testcase}.txt"
   
-  python3 test.py "log/${testcase}.txt" "$2" "$3" "$4" "$5" "$6"
+  python3 test.py "log/${testcase}.txt" "$1" "$2" "$3" "$4" "$5"
   result=$?
 
   cat "log/${testcase}.txt" | grep died > /dev/null
@@ -52,15 +36,17 @@ function CHECK_LIVE() {
   fi
 
   if [ "$result" = "0" ]; then
-    echo -e "${GREEN}OK:${NC} ${testcase}"
+    echo -e "${GREEN}LIVE:${NC} $1 $2 $3 $4 $5"
   else
-    echo -e "${RED}KO:${NC} ${testcase}"
+    echo -e "${RED}DIED:${NC} $1 $2 $3 $4 $5"
   fi
 }
 
 rm -rf log
 mkdir log
 
-# CHECK normal 2 510 200 200 10
+CHECK_LIVE 5 800 200 200 7
+CHECK_LIVE 4 410 200 200 10
 
-CHECK_DIE one_philo 1 800 200 200 10
+CHECK_DIED 1 800 200 200 1000
+CHECK_DIED 4 310 200 100 1000
