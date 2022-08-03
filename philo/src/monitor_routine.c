8daa5ed1795082ce0172623a27a4d84f36189391
osorosia <64416398+osorosia@users.noreply.github.com>
2022-08-03 10:46:26 +0900

    add `ft_`

--- philo/src/monitor_routine.c
+++ philo/src/monitor_routine.c
@@ -12,32 +12,32 @@ void *monitor_routine(void *arg) {
         if (rule->eat_count > 0)
             eat_ok = true;
         while (i < rule->philo_num) {
-            pthread_mutex_lock(&table->eat_count_mutex[i]);
+            ft_pthread_mutex_lock(&table->eat_count_mutex[i]);
             if (philos[i]->eat_count < rule->eat_count)
                 eat_ok = false;
-            pthread_mutex_unlock(&table->eat_count_mutex[i]);
+            ft_pthread_mutex_unlock(&table->eat_count_mutex[i]);
 
-            pthread_mutex_lock(&table->eat_time_mutex[i]);
+            ft_pthread_mutex_lock(&table->eat_time_mutex[i]);
             if (philos[i]->prev_eat_time == 0 &&
                     get_timestamp() - table->start_time >= rule->die ||
                 philos[i]->prev_eat_time > 0 &&
                     get_timestamp() - philos[i]->prev_eat_time >= rule->die) {
-                pthread_mutex_lock(&table->print_mutex);
+                ft_pthread_mutex_lock(&table->print_mutex);
                 table->finish = true;
                 printf("%ld\t%ld died\n", get_timestamp() - table->start_time,
                        i + 1);
-                pthread_mutex_unlock(&table->print_mutex);
+                ft_pthread_mutex_unlock(&table->print_mutex);
                 end = true;
             }
-            pthread_mutex_unlock(&table->eat_time_mutex[i]);
+            ft_pthread_mutex_unlock(&table->eat_time_mutex[i]);
             if (end)
                 return NULL;
             i++;
         }
         if (eat_ok) {
-            pthread_mutex_lock(&table->print_mutex);
+            ft_pthread_mutex_lock(&table->print_mutex);
             table->finish = true;
-            pthread_mutex_unlock(&table->print_mutex);
+            ft_pthread_mutex_unlock(&table->print_mutex);
             return NULL;
         }
         usleep(10);
